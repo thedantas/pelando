5baf4146770d566fb0cf9d7e49d7bb15882ef409
Andre Dantas <andrepopPB@gmail.com>
2024-03-17 15:06:35 -0300

    Update video_list_viewmodel.dart

--- pelando_play/lib/viewmodels/video_list_viewmodel.dart
+++ pelando_play/lib/viewmodels/video_list_viewmodel.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import '../models/video_model.dart';
 import '../services/youtube_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
+/// ViewModel que gerencia o estado da lista de vídeos.
 class VideoListViewModel with ChangeNotifier {
   final YoutubeService _youtubeService = YoutubeService();
   List<VideoModel> _videos = [];
 
-  List<VideoModel> get videos => _videos;
+  /// Retorna a lista imutável de vídeos.
+  List<VideoModel> get videos => List.unmodifiable(_videos);
 
-Future<List<VideoModel>> fetchVideos(String query) async {
-  try {
-    List<VideoModel> videos = await _youtubeService.fetchVideos(query);
-    return videos; // Retorna a lista temporária para o modal
-  } catch (e) {
-    print('Erro ao buscar vídeos: $e');
-    throw e; // Propaga o erro para ser tratado pelo FutureBuilder
+  /// Busca vídeos com base na query fornecida e retorna uma lista de VideoModel.
+  /// 
+  /// Se a busca falhar, propaga a exceção para ser tratada pelo chamador.
+  Future<List<VideoModel>> fetchVideos(String query) async {
+    try {
+      List<VideoModel> videos = await _youtubeService.fetchVideos(query);
+      return videos; // Retorna a lista temporária para o modal.
+    } catch (e) {
+      print('Erro ao buscar vídeos: $e');
+      throw e; // Propaga o erro para ser tratado pelo FutureBuilder.
+    }
   }
-}
-  // Adiciona um vídeo à lista.
+
+  /// Adiciona um vídeo à lista baseado na URL fornecida.
+  /// 
+  /// Se a adição falhar, loga o erro.
   Future<void> addVideoByUrl(String url) async {
     try {
       final video = await _youtubeService.fetchVideoByUrl(url);
@@ -31,21 +38,21 @@ Future<List<VideoModel>> fetchVideos(String query) async {
     }
   }
 
-// void clearVideos() {
-//   _videos = [];
-//   notifyListeners();
-// }
+  /// Limpa a lista de vídeos.
+  void clearVideos() {
+    _videos.clear();
+    notifyListeners();
+  }
 
-  // Método para adicionar um vídeo diretamente à lista.
+  /// Adiciona um vídeo diretamente à lista.
   void addVideo(VideoModel video) {
     _videos.add(video);
     notifyListeners();
   }
 
-  // Método para remover um vídeo da lista.
+  /// Remove um vídeo da lista.
   void removeVideo(VideoModel video) {
     _videos.removeWhere((v) => v.id == video.id);
     notifyListeners();
   }
-
 }
